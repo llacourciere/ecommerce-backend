d40866d4ac11879ba914473e796bfc00be318565
LaLacou <108777796+users.noreply.github.com>
2022-10-11 17:07:27 -0700

    added update, create and delete for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -42,16 +42,58 @@ router.get('/:id', (req, res) => {
   })
 });
 
-router.post('/', (req, res) => {
+router.post('/', ({body}, res) => {
   // create a new tag
+  Tag.create({
+    ...body
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({ 
+    tag_name: req.body.tag_name
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  }
+  )
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({message: 'No such tag found'})
+      return;
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag exists with this id'});
+    }
+    res.json(dbTagData)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
